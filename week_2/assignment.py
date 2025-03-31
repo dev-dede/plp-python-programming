a97024eed4c0171a855f848f8d84f647a824f779
dev-dede <tsewayog@gmail.com>
2025-03-31 17:40:58 +0000

    Refine code and add comments

--- week_2/assignment.py
+++ week_2/assignment.py
@@ -1,20 +1,18 @@
 my_list = []
 
-my_list = [10, 20, 30, 40]
+my_list = [10, 20, 30, 40] #Can use .append to append one after the other
 print(my_list)
 
 my_list.insert(1, 15)
 print(my_list)
 
-new_list = [50, 60, 70]
-
-my_list.extend(new_list)
+my_list.extend([50, 60, 70]) #Directly put the list in the function
 print(my_list)
 
-del my_list[-1]
+del my_list[-1] #Could also use my_list.pop() to specifically remove the last item
 print(my_list)
 
-my_list.sort(reverse=True)
+my_list.sort(reverse=True) #Decided to sort in descending order since list is already in ascending order
 print(my_list)
 
 print(my_list.index(30))
